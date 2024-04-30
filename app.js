54c8c9779c60cfa2113282e0adae51837f1799d6
Tomas Ferreras <hellotomasdev@gmail.com>
2024-04-30 19:20:39 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT;
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
+
+app.post("/api/contact", (req, res) => {
+  console.log(req.body);
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: "hellotomasdev@gmail.com",
+    subject: "Hey Tomas, you have a new message from your website!",
+    text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nMessage: ${req.body.message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
+    }
+  });
+});
+
+module.exports = app;
